3f7441a110cc6127537ad5a62ad902a4880e70aa
person - DZ84 <gzuz13@hotmail.com>
2018-11-13 06:28:25 +0000

    added some code to the gunicorn starting options to make it more convenient during dev. (max requests=1)

--- overhead/start.sh
+++ overhead/start.sh
@@ -33,14 +33,20 @@ function start_production() {
 	#exec gunicorn config.wsgi -w 4 -b 0.0.0.0:8004 --log-file=- 
 
 	
-	# when workers are silent for a certain period, they restart. -t specifies
-	# the length of this period. Enabling you to use pdb in the meantime.
-	exec gunicorn config.wsgi -w 4 -b 0.0.0.0:8004 --log-file=- -t 90000
+	# DEBUG, for explanations read below
+	exec gunicorn config.wsgi -w 4 -b 0.0.0.0:8004 --log-file=- -t 90000 --max-requests 1 
 
-	
+
+	# for pdb: when workers are silent for a certain period, they restart. -t specifies
+	# the length of this period. Enabling you to use pdb in the meantime. 90000 seconds
+	# should be enough.
+	# -t 90000
 	
 	# not ment for production, reload is suppose to reload workers when code changes
 	# --reload --preload 
+	# but I prefer the next option, it gives (much?) more insurance that the new load
+	# runs on the new code
+	# --max-requests 1
 
 
 	# gunicorn config.wsgi -w 4 -b 'unix:///tmp/gunicorn1.sock' --log-file=- 
