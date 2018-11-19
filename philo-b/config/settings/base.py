66a844563dcf7a752b684a23e5e892788e8f6380
person - DZ84 <gzuz13@hotmail.com>
2018-11-19 18:46:09 +0000

    styles and js are now added als static, and are served as such by nginx. also removed some clutter here and there.

--- philo-b/config/settings/base.py
+++ philo-b/config/settings/base.py
@@ -54,7 +54,6 @@ DATABASES = {
     }
 }
 
-
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 # URLS
@@ -170,18 +169,22 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+# STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+
+# STATIC_ROOT = str(ROOT_DIR('philo-b/static'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
-]
+# STATICFILES_DIRS = [
+#     #str(ROOT_DIR.path('philo-b/static/')),
+#     str(APPS_DIR.path('static')),
+# ]
+
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# ]
 
 # MEDIA
 # ------------------------------------------------------------------------------
---
+++ philo-b/philo-b/static/js/blog/post.js
@@ -0,0 +1,69 @@
+function create_subcomment_form(parent_nr) {
+
+	var new_subcomment = create_new_subcomment(parent_nr)
+
+	var parent_element = document.getElementById("comment-" + parent_nr)
+	var new_subcomment_buttons = parent_element
+		.getElementsByClassName("button-container")[0]
+
+	parent_element.insertBefore(
+		new_subcomment,
+		new_subcomment_buttons
+	)
+
+	delete_create_buttons(new_subcomment_buttons, parent_nr)
+}
+
+function create_new_subcomment(parent_nr) {
+	// -create form by cloning
+	// -give correct id_parent_comment to prevent duplicate id's
+	// -clear textarea from cloned form
+	// -csrf token value is not copied with the cloning
+	// -adjust info value as it will be used upon submission
+
+	new_subcomment = document.getElementById("comment-default").cloneNode(true)
+	new_subcomment.id = "reply-to-" + parent_nr
+
+	new_textarea = new_subcomment.querySelectorAll("textarea")[0]
+	new_textarea.id = "text-area-" + parent_nr
+	new_textarea.value = "" 
+
+	new_input_all = new_subcomment.querySelectorAll("input")
+
+	new_input_csrf = new_input_all[0]
+	new_input_csrf.value = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+	new_input_info = new_input_all[1]
+	new_input_info.id = "parent-comment-" + parent_nr
+	new_input_info.value = parent_nr	
+
+	return new_subcomment
+}
+
+function delete_create_buttons(new_subcomment_buttons, parent_nr) {
+	// -remove reply button
+	// -configure submit button
+	// -insert submit button
+
+	new_subcomment_buttons.getElementsByClassName('reply')[0].remove()
+
+	var new_submit_button = document.createElement('BUTTON')
+
+	new_submit_button.addEventListener('click', function() {
+		submit_subcomment_form(parent_nr)
+	})
+	new_submit_button.className = "submit"
+	new_submit_button.innerHTML = "submit"
+
+	new_subcomment_buttons
+		.appendChild(new_submit_button)
+}
+
+function submit_subcomment_form(parent_nr) {
+	document.getElementById("reply-to-" + parent_nr).submit()
+}
+
+function submit_comment_form() {
+	document.getElementById("comment-default").submit()
+}
+
